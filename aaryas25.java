2ccc7d16e596ce3a5b6115d568c766ba9588f9e6
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-08-02 17:37:22 +0530

    Add files via upload

---
+++ aaryas25.java
@@ -0,0 +1,17 @@
+package july;
+
+public class aaryas25 
+{
+	public static void main(String []args)
+	{
+		System.out.println("Enter the number");
+		int[] marks= new int[]{22,23,24,24,26};
+		for(int i=0; i<marks.length; i++)
+		{
+			System.out.println(marks[i]);
+		}
+		
+		
+	}
+
+}
