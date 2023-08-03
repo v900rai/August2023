4ad05fd2e788ab6b8b8be3f958804afa07936454
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-08-03 18:23:44 +0530

    Add files via upload

---
+++ sumTwoNumber.java
@@ -0,0 +1,21 @@
+package august200;
+
+import java.util.Scanner;
+
+public class sumTwoNumber 
+{
+	public static void main(String []args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number1");
+		int num1=sc.nextInt();
+		System.out.println("Enter the number2");
+		int num2=sc.nextInt();
+		
+		int result=num1+num2;
+		System.out.println(result);
+		
+		
+	}
+
+}
