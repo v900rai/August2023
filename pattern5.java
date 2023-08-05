640e461fa2410a8b41d33c63c397a3ff69a0b063
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-08-05 23:58:17 +0530

    Add files via upload

---
+++ pattern5.java
@@ -0,0 +1,37 @@
+package august2023Target200;
+
+import java.util.Scanner;
+
+public class pattern5 
+{
+	public static void main(String []args)
+	{
+		Scanner sc=new Scanner(System.in);
+		  //Taking rows value from the user
+        
+        System.out.println("How many rows you want in this pattern?");
+         
+        int rows = sc.nextInt();
+         
+        System.out.println("Here is your pattern....!!!");
+			for(int i=rows; i>=1; i--)
+			{
+				for(int j=1; j<=i; j++)
+				{
+					System.out.print(j+"");
+				}
+				System.out.println();
+			}
+			//printin the elements
+			for(int i=2; i<=rows; i++)
+			{
+				for(int j=1; j<=i; j++)
+				{
+					System.out.print(j+"");
+				}
+				System.out.println();
+			}
+	}
+	
+
+}
