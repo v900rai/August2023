81e0a97297087d6d99f823cc29227870ea043a3d
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-08-10 14:06:32 +0530

    Add files via upload

---
+++ equalStringProgram.java
@@ -0,0 +1,14 @@
+package javaInterviewProgram;
+
+public class equalStringProgram 
+{
+	public static void main(String []args)
+	{
+		String s1="vishal";
+		String s2="vishal";
+		String s3=new String("vishal");
+		//System.out.println(s1=s2);
+		System.out.print(s1.equals(s3));
+	}
+
+}
